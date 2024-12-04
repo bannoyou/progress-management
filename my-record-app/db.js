3cbd9b1553270a3e73b8bc71480acd4c712d47d2
bannoyou <115530238+bannoyou@users.noreply.github.com>
2024-12-04 11:56:08 +0900

    updata my-record-app

---
+++ my-record-app/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost', // MySQLホスト名
+    user: 'root', // MySQLユーザー名
+    password: 'youi0819', // MySQLのパスワード
+    database: 'progress', // 使用するデータベース名
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+module.exports = pool;
