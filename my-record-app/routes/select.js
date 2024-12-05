10117e717d6092c1d427e7ed6398e59c48e5011e
bannoyou <115530238+bannoyou@users.noreply.github.com>
2024-12-05 18:51:58 +0900

    updata

--- my-record-app/routes/select.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pool = require('../db'); // MySQL接続用プール
-
-router.get('/', (req, res) => {
-    res.render('select');  // register.ejsを表示する処理
-});
-
-module.exports = router;
